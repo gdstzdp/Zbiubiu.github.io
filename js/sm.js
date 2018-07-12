b7d28b84b6f9dfb14cb1683d55650382e99aa2bd
biubiu <zdepiao@163.com>
2018-07-12 23:57:22 +0800

    Test

---
+++ js/sm.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+$(document).ready(function () {
+
+    //
+    // //获取要定位元素距离浏览器顶部的距离
+    // var navH = $(".header").offset().top+80;
+    // //滚动条事件
+    // $(window).scroll(function(){
+    //     //获取滚动条的滑动距离
+    //     var scroH = $(this).scrollTop();
+    //     //滚动条的滑动距离大于等于定位元素距离浏览器顶部的距离，就固定，反之就不固定
+    //     if(scroH>=navH){
+    //         $(".header").css({"position":"fixed"});
+    //         $(".header").addClass("top")
+    //     }else if(scroH<navH){
+    //         $(".header").css({"position":"absolute"});
+    //         $(".header").removeClass("top")
+    //     }
+    //
+    // });
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
